e65ecebf2f634021e38e97e1878eb2bc608b21ef
Dan <57072903+tuffstuff9@users.noreply.github.com>
2022-08-09 15:36:31 +0300

    Added demo

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace KeyboardHookLite;
 
 internal class Program
 {
-
+    // Here is a demo of the library in action
     static void Main(string[] args)
     {
         KeyboardHook kb = new KeyboardHook();
@@ -18,6 +18,28 @@ internal class Program
 
     private static void Kb_KeyboardPressed(object? sender, KeyboardHookEventArgs e)
     {
+        // Outputs the pressed type of key press.
+        // WM_KEYDOWN, WM_SYSKEYDOWN, WM_KEYUP, WM_SYSKEYUP
+        Console.WriteLine(e.KeyPressType);
+
+        // Outputs the pressed Key (of type System.Windows.Input).
         Console.WriteLine(e.InputEvent.Key);
+
+        // Outputs the virtual key code.
+        Console.WriteLine(e.InputEvent.VirtualCode);
+
+        // Outputs a hardware scan code for the key. 
+        Console.WriteLine(e.InputEvent.HardwareScanCode);
+
+        // Outputs the extended-key flag, event-injected Flags,
+        // context code, and transition-state flag.
+        Console.WriteLine(e.InputEvent.Flags);
+
+        // Outputs the time stamp stamp for this message, equivalent
+        // to what GetMessageTime would return for this message.
+        Console.WriteLine(e.InputEvent.TimeStamp);
+
+        // Outputs additional information associated with the message. 
+        Console.WriteLine(e.InputEvent.AdditionalInformation);
     }
 }
